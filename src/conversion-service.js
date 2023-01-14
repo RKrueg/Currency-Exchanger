b6e959d41f89199ef2d2af772d80fe98b73462f7
RKrueg <thegckid@gmail.com>
2023-01-13 19:58:07 -0800

    fix URL to call on correct currency

--- src/conversion-service.js
+++ src/conversion-service.js
@@ -2,7 +2,7 @@ export default class ConversionService {
   static getCurrency(userAmount) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/CNH`;
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
       request.addEventListener("loadend", function() {
         const response = JSON.parse(this.responseText);
         if (this.status === 200) {
