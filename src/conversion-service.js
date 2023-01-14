a576d9cad39df00a14f32f68da82dfa5322867da
RKrueg <thegckid@gmail.com>
2023-01-13 19:38:03 -0800

    make code more readable by updating name usd to userAmount

--- src/conversion-service.js
+++ src/conversion-service.js
@@ -1,14 +1,14 @@
 export default class ConversionService {  
-  static getCurrency(usd) {
+  static getCurrency(userAmount) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
       request.addEventListener("loadend", function() {
         const response = JSON.parse(this.responseText);
         if (this.status === 200) {
-          resolve([response, usd]);
+          resolve([response, userAmount]);
         } else {
-          reject([this, response, usd]);
+          reject([this, response, userAmount]);
         }
       });
       request.open("GET", url, true);
